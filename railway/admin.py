5db25bf07a676293680e264cda06461bde65f422
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-02-19 14:03:53 +0530

    first commit

---
+++ railway/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *;
+# Register your models here.
+
+admin.site.register(Register)
+admin.site.register(Add_Train)
+admin.site.register(Add_route)
+admin.site.register(Passenger)
+admin.site.register(Book_ticket)
+
