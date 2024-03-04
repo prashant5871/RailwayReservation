5f4485b4f8ccb53e74fde3411c7b66eda8a6227e
mihir <mihirkarangiya778@gmail.com>
2024-03-04 13:50:24 +0530

    testing

--- railway/urls.py
+++ railway/urls.py
@@ -5,4 +5,5 @@ from django.conf import settings
 
 urlpatterns=[
     path('',views.home,name="home"),
+    path('login',views.login_user,name="login"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- railway/views.py
+++ railway/views.py
@@ -1,7 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.models import User
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request,'index.html')
+
+def login_user(request):
+
+    # error = False
+    # valid = False
+
+    # if request.method == "POST":
+    #     name = request.POST["username"]
+    #     pwd = request.POST["password"]
+
+    #     try:
+    #         user = authenticate(username = name, password = pwd)
+    #     except:
+    #         error = True   
+    #     try:
+    #         if user:
+    #             login(request,user)
+    #             valid = True
+    #     except:
+    #         error = True
+    
+    # dic = {'error':error,'valid':valid}
+
+    return render(request,'login.html')
